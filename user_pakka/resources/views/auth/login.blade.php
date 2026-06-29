c908b5d5315829c4bf8287e6b84a7c7e10efc288
Sosethika Kou <skou2@paragoniu.edu.kh>
2026-06-30 03:19:10 +0700

    font

--- user_pakka/resources/views/auth/login.blade.php
+++ user_pakka/resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
 <title>Pakka Login</title>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Battambang:wght@300;400;700&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 <script src="{{ asset('js/auth/login.js') }}" defer></script>
 
